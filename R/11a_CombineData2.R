2443ab710f401b7a828e2be3e2f820d405d2b28f
Patrick Schaefer <patrick.schaefer@mail.utoronto.ca>
2023-05-31 11:19:10 -0400

    rename

--- R/11a_CombineData2.R
+++ R/11a_CombineData2.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(openxlsx)
+library(sf)
 
 lu_master<-readRDS(file.path("data","lookups.rds"))
 
@@ -17,22 +18,24 @@ ep_data<-readxl::read_excel(file.path("data","Processed","Bio","Fish_Endpoints.x
   left_join(sub_region_out) %>% 
   left_join(matched_points)
 
-write_csv(ep_data,file.path("data","final","Model_building_endpoint_data.csv"))
+write_csv(ep_data,file.path("data","final","Model_building_rawendpoint_data.csv"))
 
 tx_data<-readxl::read_excel(file.path("data","Processed","Bio","Fish_Endpoints.xlsx"),"Taxa Table") %>% 
   mutate(SampleDate=as.Date(SampleDate)) %>% 
   left_join(sub_region_out) %>% 
   left_join(matched_points)
 
-write_csv(tx_data,file.path("data","final","Model_building_taxa_data.csv"))
+write_csv(tx_data,file.path("data","final","Model_building_rawtaxa_data.csv"))
 
-wb = createWorkbook()
-
-addWorksheet(wb, "Endpoint Dataset")
-addWorksheet(wb, "Taxa Dataset")
-
-writeData(wb,ep_data, sheet="Endpoint Dataset", rowNames =FALSE)
-writeData(wb,tx_data, sheet="Taxa Dataset", rowNames =FALSE)
-
-saveWorkbook(wb, file.path("data","final","Model_building_data.xlsx"),overwrite = T)
+if (F) {
+  wb = createWorkbook()
+  
+  addWorksheet(wb, "Endpoint Dataset")
+  addWorksheet(wb, "Taxa Dataset")
+  
+  writeData(wb,ep_data, sheet="Endpoint Dataset", rowNames =FALSE)
+  writeData(wb,tx_data, sheet="Taxa Dataset", rowNames =FALSE)
+  
+  saveWorkbook(wb, file.path("data","final","Model_building_data.xlsx"),overwrite = T)
+}
 
